925b064bef85a04e9971cfcbc14af97f14afeb84
electrodrigo <rodrigo.carrasco.davis@gmail.com>
2018-12-03 12:20:52 -0300

    doing data manager

--- data_manager.py
+++ data_manager.py
@@ -17,6 +17,37 @@ class DataManager(object):
         print("molecules traces found", len(self.molecule_list))
         print("gold traces found", len(self.gold_list))
 
+        file = open(self.molecule_path + self.molecule_list[0], "r")
+
+        self.convert_file(file)
+
+    def convert_file(self, file):
+        metadata = {}
+        forward_trace = []
+        backward_trace = []
+        separation1_found = False
+        separation2_found = False
+        for i, line in enumerate(file):
+            if line[0] != "@" and not separation1_found:
+                if line.split(" ")[0] == "sampling":
+                    meta_name = line.split("=")[0]
+                    meta_name = meta_name[:-1].replace(" ", "_")
+                    meta_value = line.split("=")[1].split(" ")[1]
+                    metadata[meta_name] = np.float(meta_value)
+                else:
+                    meta_name = line.split(":")[0]
+                    meta_name = meta_name[:-1].replace(" ", "_")
+                    meta_value = line.split(":")[1].split(" ")[1]
+                    metadata[meta_name] = np.float(meta_value)
+            elif line[0] == "@":
+                separation1_found = True
+                #print(line)
+                continue
+            if separation1_found:
+                row = np.array(line.split("\t")[:-1])
+                forward_trace.append(row.astype(np.float))
+
+
 if __name__ == "__main__":
 
     molecule_path = "../data/molecule/"
